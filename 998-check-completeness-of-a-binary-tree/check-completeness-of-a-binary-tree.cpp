93dc2c411d98045a47d3bd49c0eedaa44a2876a5
DEVJHAWAR11 <156332863+DEVJHAWAR11@users.noreply.github.com>
2025-05-06 14:42:42 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (9.15%) - LeetSync

--- 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
@@ -9,24 +9,51 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+ //approach1
+// class Solution {
+// public:
+//     bool isCompleteTree(TreeNode* root) {
+//         //ek null ke baad ek notnull nhi aana chahiye agar aaya toh not complete
+//         //toh keep adding in queue jab tak pehla null nhi mil jaata
+//         //after that keep popping all null from queue
+//         //if at last we get a empty queue -->then complete else not
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         while(q.front()){
+//             TreeNode* node=q.front();
+//             q.pop();
+//             q.push(node->left);
+//             q.push(node->right);
+//         }
+//         while(!q.empty()&& !q.front()){
+//             q.pop();
+//         }
+//         return q.empty();
+//     }
+// };
+
+
+//aproach 2
+//if we get null then we mark a flag variable and then if we get a not null then and if flag is marked then return false  
 class Solution {
 public:
     bool isCompleteTree(TreeNode* root) {
-        //ek null ke baad ek notnull nhi aana chahiye agar aaya toh not complete
-        //toh keep adding in queue jab tak pehla null nhi mil jaata
-        //after that keep popping all null from queue
-        //if at last we get a empty queue -->then complete else not
+        bool flag=false;
         queue<TreeNode*>q;
         q.push(root);
-        while(q.front()){
+
+        while(!q.empty()){
             TreeNode* node=q.front();
             q.pop();
-            q.push(node->left);
-            q.push(node->right);
+            if(node==NULL) flag=true;
+            else{    //jab not null aaye
+                if(flag) return false;   //flag tab hi true hoga agar ek null a gya hoga
+
+                q.push(node->left);
+                q.push(node->right);
+            }
         }
-        while(!q.empty()&& !q.front()){
-            q.pop();
-        }
-        return q.empty();
+        return true;
     }
 };
