2c937954629be1f80cd1a964f9ea54afd0d6ac4c
DEVJHAWAR11 <156332863+DEVJHAWAR11@users.noreply.github.com>
2025-05-12 11:07:06 +0530

    Time: 3 ms (64.31%) | Memory: 8.4 MB (5.01%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,10 +1,32 @@
+// class Solution {
+// public:
+//     int fib(int n) {
+        
+//         if(n==0 || n==1) return n;
+
+//         int ans=fib(n-1)+fib(n-2);
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    int fib(int n) {
-        
+    int solveRecMemo(int n , vector<int>&dp){
+        //base case
         if(n==0 || n==1) return n;
+        
+        //step3: if already solved then return it
+        if(dp[n]!=-1) return dp[n]; 
 
-        int ans=fib(n-1)+fib(n-2);
+        //step2: store and return dp
+        dp[n]= solveRecMemo(n-1,dp)+solveRecMemo(n-2,dp);
+        return dp[n]; 
+    }
+
+    int fib(int n) {
+        //step1: Create a DP array
+        vector<int>dp(n+1,-1);
+        int ans=solveRecMemo(n,dp);
         return ans;
     }
 };
