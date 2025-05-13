eb3c24fae6cd910342528e6ff99b01c26cdf02cc
DEVJHAWAR11 <156332863+DEVJHAWAR11@users.noreply.github.com>
2025-05-13 08:07:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (62.89%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -17,25 +17,54 @@
 //     }
 // };
 
+//Memoisation
+// class Solution {
+// public:
+//     int solveMemoisation(vector<int>&nums,vector<int>&dp , int i){
+//         int include=0,exclude=0;
+//         //base case
+//         if(i >= nums.size()) return 0;
+
+//         if(dp[i]!=-1) return dp[i];
+
+//         include=nums[i] + solveMemoisation(nums,dp,i+2);
+//         exclude = 0 + solveMemoisation(nums,dp,i+1);
+//         dp[i] = max(include , exclude);
+//         return dp[i];
+//     }
+
+//     int rob(vector<int>& nums) {
+//         int i=0;  
+//         vector<int>dp(nums.size(),-1);
+//         int ans=solveMemoisation(nums,dp,i);
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
-    int solveMemoisation(vector<int>&nums,vector<int>&dp , int i){
+    int solveTabulation(vector<int>&nums,vector<int>&dp){
         int include=0,exclude=0;
-        //base case
-        if(i >= nums.size()) return 0;
 
-        if(dp[i]!=-1) return dp[i];
+        if(nums.size()==1) return nums[0];
 
-        include=nums[i] + solveMemoisation(nums,dp,i+2);
-        exclude = 0 + solveMemoisation(nums,dp,i+1);
-        dp[i] = max(include , exclude);
-        return dp[i];
+        dp[0]=nums[0];
+        dp[1]=max(nums[1],nums[0]);     //to handle case when two elemenst and first > second
+
+        for(int i=2;i<nums.size();i++){
+            include=nums[i] + dp[i-2];
+            exclude = 0 + dp[i-1];
+            dp[i] = max(include , exclude);
+        }
+        
+        return dp[nums.size()-1];
     }
 
-    int rob(vector<int>& nums) {
-        int i=0;  
+    int rob(vector<int>& nums) { 
         vector<int>dp(nums.size(),-1);
-        int ans=solveMemoisation(nums,dp,i);
+        int ans=solveTabulation(nums,dp);
         return ans;
     }
 };
+
