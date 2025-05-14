9e32c0576b97970f43fe1c1b7bf70eb0ba23c2e2
DEVJHAWAR11 <156332863+DEVJHAWAR11@users.noreply.github.com>
2025-05-14 06:04:38 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (87.04%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -30,20 +30,33 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int usingTabu(int n,vector<int>&dp){
+//         dp[0]=1;
+//         dp[1]=1;
+//         if(n==0 || n==1) return dp[0];
+//         for(int i=2;i<=n;i++){
+//             dp[i]=dp[i-1]+dp[i-2];
+//         }
+//         return dp[n];
+//     }
+
+//     int climbStairs(int n) {
+//        vector<int>dp(n+1,-1);
+//        return usingTabu(n,dp); 
+//     }
+// };
+
 class Solution {
 public:
-    int usingTabu(int n,vector<int>&dp){
-        dp[0]=1;
-        dp[1]=1;
-        if(n==0 || n==1) return dp[0];
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
-    }
-
     int climbStairs(int n) {
-       vector<int>dp(n+1,-1);
-       return usingTabu(n,dp); 
+       int prev=1,curr=1;
+       for(int i=2;i<=n;i++){
+            int temp=prev+curr;
+            prev=curr;
+            curr=temp;
+       }
+       return curr;
     }
 };
