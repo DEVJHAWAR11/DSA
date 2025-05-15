23925d9389d4d4c84ea7f727c05b47c08766c322
DEVJHAWAR11 <156332863+DEVJHAWAR11@users.noreply.github.com>
2025-05-15 14:28:59 +0530

    Time: 195 ms (12.89%) | Memory: 140.7 MB (11.02%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -16,44 +16,44 @@ public:
     //     return true;
     // }
 
-    bool subsetSum(vector<int>&nums,int sum,int n,vector<vector<int>>&dp){
-        if(sum==0) return true;
-        if(n==0)  return false;
+    // bool subsetSum(vector<int>&nums,int sum,int n,vector<vector<int>>&dp){
+    //     if(sum==0) return true;
+    //     if(n==0)  return false;
 
-        if(dp[n][sum]!=-1) return dp[n][sum]; 
+    //     if(dp[n][sum]!=-1) return dp[n][sum]; 
 
-        if(nums[n-1]<=sum){
-            bool include=subsetSum(nums,sum-nums[n-1],n-1,dp);
-            bool exclude =subsetSum(nums,sum,n-1,dp);
-            return dp[n][sum]= include || exclude;
-        }
-        else{
-            return dp[n][sum]= subsetSum(nums,sum,n-1,dp);
+    //     if(nums[n-1]<=sum){
+    //         bool include=subsetSum(nums,sum-nums[n-1],n-1,dp);
+    //         bool exclude =subsetSum(nums,sum,n-1,dp);
+    //         return dp[n][sum]= include || exclude;
+    //     }
+    //     else{
+    //         return dp[n][sum]= subsetSum(nums,sum,n-1,dp);
             
-        }
-    }
-
-    // bool subsetSum(vector<int>nums,int sum,int n,vector<vector<bool>>&dp){
-    //     for(int i=0;i<n+1;i++){
-    //         for(int j=0;j<sum+1;j++){
-    //             dp[i][0]=true;
-    //         }
     //     }
-
-    //     for(int i=1;i<n+1;i++){
-    //         for(int j=1;j<sum+1;j++){
-    //             if(nums[i-1]<=j){
-    //                 bool include=dp[i-1][j-nums[i-1]];
-    //                 bool exclude =dp[i-1][j];
-    //                 dp[i][j]= include || exclude;
-    //             }
-    //             else{
-    //                 dp[i][j]= dp[i-1][j];
-    //             }
-    //         }
-    //     }
-    //     return dp[n][sum];
     // }
+
+    bool subsetSum(vector<int>nums,int sum,int n,vector<vector<int>>&dp){
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<sum+1;j++){
+                dp[i][0]=true;
+            }
+        }
+
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<sum+1;j++){
+                if(nums[i-1]<=j){
+                    bool include=dp[i-1][j-nums[i-1]];
+                    bool exclude =dp[i-1][j];
+                    dp[i][j]= include || exclude;
+                }
+                else{
+                    dp[i][j]= dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
 
     bool canPartition(vector<int>& nums) {
         int sum=0;
@@ -61,7 +61,7 @@ public:
         for(auto i : nums){
             sum+=i;
         }
-        vector<vector<int>>dp(n+1,vector<int>(sum/2+1,-1));
+        vector<vector<int>>dp(n+1,vector<int>(sum/2+1,false));
         if(sum&1) return false;
         else return subsetSum(nums,sum/2,n,dp);
     }
