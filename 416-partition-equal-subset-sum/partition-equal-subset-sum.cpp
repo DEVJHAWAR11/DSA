0b6722343b31835cccb6fbafc7b19dca7f6db456
DEVJHAWAR11 <156332863+DEVJHAWAR11@users.noreply.github.com>
2025-05-15 14:13:23 +0530

    Time: 229 ms (8.94%) | Memory: 18.9 MB (46.34%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+
+    // bool subsetSum(vector<int>nums,int sum,int n){
+    //     if(sum==0) return true;
+    //     if(n==0)  return false; 
+
+    //     if(nums[n-1]<=sum){
+    //         bool include=subsetSum(nums,sum-nums[n-1],n-1);
+    //         bool exclude =subsetSum(nums,sum,n-1);
+    //         return include || exclude;
+    //     }
+    //     else{
+    //         return subsetSum(nums,sum,n-1);
+    //     }
+    //     return true;
+    // }
+
+    // bool subsetSum(vector<int>nums,int sum,int n,vector<vector<bool>>&dp){
+    //     if(sum==0) return true;
+    //     if(n==0)  return false;
+
+    //     if(dp[n][sum]!=false) return dp[n][sum]; 
+
+    //     if(nums[n-1]<=sum){
+    //         bool include=subsetSum(nums,sum-nums[n-1],n-1,dp);
+    //         bool exclude =subsetSum(nums,sum,n-1,dp);
+    //         dp[n][sum]= include || exclude;
+    //         return dp[n][sum];
+    //     }
+    //     else{
+    //         dp[n][sum]= subsetSum(nums,sum,n-1,dp);
+    //         return dp[n][sum];
+    //     }
+    //     return dp[n][sum];
+    // }
+
+    bool subsetSum(vector<int>nums,int sum,int n,vector<vector<bool>>&dp){
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<sum+1;j++){
+                dp[i][0]=true;
+            }
+        }
+
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<sum+1;j++){
+                if(nums[i-1]<=j){
+                    bool include=dp[i-1][j-nums[i-1]];
+                    bool exclude =dp[i-1][j];
+                    dp[i][j]= include || exclude;
+                }
+                else{
+                    dp[i][j]= dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+
+    bool canPartition(vector<int>& nums) {
+        int sum=0;
+        int n=nums.size();
+        for(auto i : nums){
+            sum+=i;
+        }
+        vector<vector<bool>>dp(n+1,vector<bool>(sum/2+1,false));
+        if(sum&1) return false;
+        else return subsetSum(nums,sum/2,n,dp);
+    }
+};
