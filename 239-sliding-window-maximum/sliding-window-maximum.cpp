4bc6835700761f96f9d20400a82f4232451a1149
DEVJHAWAR11 <156332863+DEVJHAWAR11@users.noreply.github.com>
2025-06-02 19:52:01 +0530

    Time: 12 ms (95.75%) | Memory: 139.1 MB (82.85%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,47 +1,44 @@
-// class Solution {
-// public:
-//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-//         vector<int>ans;
-//         queue<int>q;
-//         int i=0,j=0;
-//         while(j < nums.size()){
-//             while(!q.empty() && nums[j]>q.front())  q.pop();
-
-//             q.push(nums[j]);
-
-//             if( j-i+1 < k)  j++;
-
-//             else if (j-i+1 == k){
-//                 ans.push_back(q.front());
-//                 if(nums[i]==q.front())
-//                     q.pop();
-//                 i++,j++;
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
+    // Function to return a vector of the maximums of each sliding window of size k
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int>ans;
-        deque<int>dq;
-        int i=0,j=0;
-        while(j < nums.size()){
-            while(!dq.empty() && nums[j]>dq.back())  dq.pop_back();
+        vector<int> ans;             // Result vector to store maximums
+        int n = nums.size();         // Total number of elements in the input
+        int left = 0, right = 0;     // Window pointers: [left, right]
 
-            dq.push_back(nums[j]);
+        deque<int> dq;               // Deque to store potential maximums in current window
+                                     // It will store elements in decreasing order from front to back
 
-            if( j-i+1 < k)  j++;
+        // Expand the window to the right until we process all elements
+        while (right < n) {
+            // Maintain deque: remove all elements smaller than current from the back
+            // Because they cannot be the maximum if current is larger
+            while (!dq.empty() && dq.back() < nums[right]) {
+                dq.pop_back();
+            }
+            // Add current element at the back of deque
+            dq.push_back(nums[right]);
 
-            else if (j-i+1 == k){
+            // If window size < k, just move right to expand
+            if (right - left + 1 < k) {
+                right++;
+            }
+            // When window size == k, record result and slide the window
+            else if (right - left + 1 == k) {
+                // The front of deque is the maximum in current window
                 ans.push_back(dq.front());
-                if(nums[i]==dq.front())
+
+                // Before sliding window, if the element leaving (nums[left])
+                // is equal to the deque's front, pop it from deque
+                if (dq.front() == nums[left]) {
                     dq.pop_front();
-                i++,j++;
+                }
+                // Slide window by incrementing both pointers
+                left++;
+                right++;
             }
         }
-        return ans;
+
+        return ans;  // Return all collected window maximums
     }
 };
