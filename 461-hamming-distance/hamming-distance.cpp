3a884e348ffdb6e7218c18bad017bd0023baf23c
DEVJHAWAR11 <156332863+DEVJHAWAR11@users.noreply.github.com>
2025-06-05 06:03:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (66.42%) - LeetSync

---
+++ 461-hamming-distance/hamming-distance.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int temp=x ^ y,ans=0;
+        while(temp){
+            ans++;
+            temp=temp&temp-1;                                
+        }
+        return ans;
+    }
+};
