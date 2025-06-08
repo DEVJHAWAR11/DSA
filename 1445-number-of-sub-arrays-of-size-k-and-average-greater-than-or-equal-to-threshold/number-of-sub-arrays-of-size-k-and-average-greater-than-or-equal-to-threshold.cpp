d7dce01d37700ad8133588d273dcbb070552a3e3
DEVJHAWAR11 <156332863+DEVJHAWAR11@users.noreply.github.com>
2025-06-08 09:41:26 +0530

    Time: 0 ms (100.00%) | Memory: 59.2 MB (66.26%) - LeetSync

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
@@ -6,11 +6,13 @@ public:
             sum+=arr[j];
             if(j-i+1 < k) j++;
             else if(j-i+1 == k){
-                if(sum/k >= threshold) count++;
+                if(sum/k >=threshold)
+                    count++;
                 sum-=arr[i];
                 i++,j++;
+
             }
         }
-        return count;
+        return count;         
     }
 };
