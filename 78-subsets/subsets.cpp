947b239edde194564706309d3238f91e26023da0
DEVJHAWAR11 <156332863+DEVJHAWAR11@users.noreply.github.com>
2025-06-17 10:32:49 +0530

    Time: 7 ms (5.74%) | Memory: 17.3 MB (6.25%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,16 +1,41 @@
+// class Solution {
+// public:
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<vector<int>>ans;
+//         vector<int>temp;
+//         for(int num=0;num<(1<<nums.size());num++){
+//             temp={};
+//             for(int i=0;i<nums.size();i++){
+//                 if(num&(1<<i))
+//                     temp.push_back(nums[i]);
+//             }
+//             ans.push_back(temp);
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
+    void solve(vector<int>ip,vector<int>op,vector<vector<int>>&res){
+        if(ip.size()==0){
+            res.push_back(op);
+            return;
+        }
+        vector<int>op1=op;
+        vector<int>op2=op;
+        op2.push_back(ip[0]);
+        ip.erase(ip.begin());
+
+        solve(ip,op1,res);
+        solve(ip,op2,res);
+    }
+
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>>ans;
-        vector<int>temp;
-        for(int num=0;num<(1<<nums.size());num++){
-            temp={};
-            for(int i=0;i<nums.size();i++){
-                if(num&(1<<i))
-                    temp.push_back(nums[i]);
-            }
-            ans.push_back(temp);
-        }
+        vector<int>op;
+        solve(nums,op,ans);
         return ans;
+        
     }
 };
