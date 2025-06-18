62f0c7f5a69d2dd7a89f66af3029491adb2865cd
DEVJHAWAR11 <156332863+DEVJHAWAR11@users.noreply.github.com>
2025-06-18 20:25:46 +0530

    Time: 14 ms (5.83%) | Memory: 20.1 MB (5.48%) - LeetSync

---
+++ 800-letter-case-permutation/letter-case-permutation.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void solve(string ip,string op,unordered_set<string>&res){
+        if(ip.size()==0){
+            res.insert(op);
+            return;
+        }
+        string op1=op;
+        string op2=op;
+        if(islower(ip[0])){
+            op1+=ip[0];
+            op2+=toupper(ip[0]);
+        }
+        else if(isupper(ip[0])){
+            op1+=ip[0];
+            op2+=tolower(ip[0]);
+        }
+        if(isdigit(ip[0])){
+            op1+=ip[0];
+            op2+=ip[0];
+        }
+        ip.erase(ip.begin());
+
+        solve(ip,op1,res);
+        solve(ip,op2,res);
+    }
+
+    vector<string> letterCasePermutation(string s) {
+        unordered_set<string>res;
+        string op="";
+        solve(s,op,res);
+        vector<string>ans(res.begin(),res.end());
+        return ans;
+    }
+};
