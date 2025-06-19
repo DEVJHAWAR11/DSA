7f1f4d09204e794a2ab054e0276b974f2bc22951
DEVJHAWAR11 <156332863+DEVJHAWAR11@users.noreply.github.com>
2025-06-19 19:54:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (52.57%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,23 +1,43 @@
-class Solution {
-private:
-    queue<int>q;
-public:
-    int findTheWinner(int n, int k) {
+// class Solution {
+// private:
+//     queue<int>q;
+// public:
+//     int findTheWinner(int n, int k) {
 
-        for(int i=1;i<=n;i++){
-            q.push(i);
-        }
+//         for(int i=1;i<=n;i++){
+//             q.push(i);
+//         }
         
-        while(q.size()!=1){
+//         while(q.size()!=1){
 
-            for(int i=1;i<k;i++){
-                q.push(q.front());
-                q.pop();
-            }
+//             for(int i=1;i<k;i++){
+//                 q.push(q.front());
+//                 q.pop();
+//             }
 
-            q.pop();
-        }
+//             q.pop();
+//         }
 
-        return q.front();
+//         return q.front();
+//     }
+// };
+
+class Solution {
+public:
+    int solve(vector<int>&temp,int k,int index){
+        if(temp.size()==1)
+            return temp[0];
+
+        index=(index+k-1)%temp.size();
+        temp.erase(temp.begin()+index);
+
+        return solve(temp,k,index);
+    }
+
+    int findTheWinner(int n, int k) {
+        vector<int>temp;
+        for(int i=1;i<=n;i++)
+            temp.push_back(i);
+        return solve(temp,k,0);
     }
 };
