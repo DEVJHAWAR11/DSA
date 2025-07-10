0701be58e4d2a480d63055d66e20b72fd41208b9
DEVJHAWAR11 <156332863+DEVJHAWAR11@users.noreply.github.com>
2025-07-10 23:37:18 +0530

    Time: 2 ms (73.55%) | Memory: 11.4 MB (82.04%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,54 +1,56 @@
-// class Solution {   
+// class Solution {
 // public:
-//     void solve(vector<int>&nums,int start,vector<vector<int>>&ans,unordered_map<int,int>&mpp){
-//         if(start==nums.size()-1){
+//     void solve(vector<int>& nums, int start, vector<vector<int>>& ans,vector<bool>&used) {
+//         if (start == nums.size()) {
 //             ans.push_back(nums);
 //             return;
 //         }
 
-//         for(int i=start;i<nums.size();i++){
-//             if(mpp.find(nums[i])==mpp.end()){
-//                 mpp[nums[i]]=1;
-//                 swap(nums[start],nums[i]);
-//                 solve(nums,start+1,ans,mpp);
-//                 swap(nums[start],nums[i]);
-//             }
-            
+//         unordered_set<int> used;  // Set for current recursion level
+
+//         for (int i = start; i < nums.size(); i++) {
+//             if (used.count(nums[i])) continue;  // Skip duplicates
+//             used.insert(nums[i]);
+
+//             swap(nums[start], nums[i]);
+//             solve(nums, start + 1, ans);
+//             swap(nums[start], nums[i]);
 //         }
 //     }
 
 //     vector<vector<int>> permuteUnique(vector<int>& nums) {
-//         vector<vector<int>>ans;
-//         unordered_map<int,int>mpp;
-//         solve(nums,0,ans,mpp);
+//         vector<vector<int>> ans;
+//         sort(nums.begin(), nums.end());  // Optional: helps group duplicates
+//         solve(nums, 0, ans);
 //         return ans;
 //     }
 // };
 
 class Solution {
 public:
-    void solve(vector<int>& nums, int start, vector<vector<int>>& ans) {
-        if (start == nums.size()) {
-            ans.push_back(nums);
+    void solve(vector<int>& nums, vector<vector<int>>& ans,vector<bool>&used,vector<int>&path) {
+        if (path.size() == nums.size()) {
+            ans.push_back(path);
             return;
         }
 
-        unordered_set<int> used;  // Set for current recursion level
-
-        for (int i = start; i < nums.size(); i++) {
-            if (used.count(nums[i])) continue;  // Skip duplicates
-            used.insert(nums[i]);
-
-            swap(nums[start], nums[i]);
-            solve(nums, start + 1, ans);
-            swap(nums[start], nums[i]);
+        for (int i = 0; i < nums.size(); i++) {
+            if (used[i]) continue; 
+            if(i>0 && nums[i-1]==nums[i] && !used[i-1]) continue;
+            used[i]=true;
+            path.push_back(nums[i]);
+            solve(nums, ans, used, path);
+            path.pop_back();
+            used[i]=false;
         }
     }
 
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>> ans;
+        vector<bool>used(nums.size(),false);
+        vector<int>path;
         sort(nums.begin(), nums.end());  // Optional: helps group duplicates
-        solve(nums, 0, ans);
+        solve(nums, ans, used, path);
         return ans;
     }
 };
