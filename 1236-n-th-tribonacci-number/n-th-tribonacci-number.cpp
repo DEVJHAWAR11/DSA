e6e65c1ee36a2f8d31a1b6cffb3f846eccfd8d0a
DEVJHAWAR11 <156332863+DEVJHAWAR11@users.noreply.github.com>
2025-07-17 15:40:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (84.74%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,13 +1,28 @@
+// class Solution {
+// public:
+//     int tribonacci(int n) {
+//         vector<int>dp(n+1,0);
+//         if(n==1 || n==2) return 1;
+//         if(n==0) return 0;
+//         dp[1]=1,dp[2]=1;
+//         for(int i=3;i<n+1;i++){
+//             dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+//         }
+//         return dp[n];
+//     }
+// };
+
 class Solution {
 public:
     int tribonacci(int n) {
-        vector<int>dp(n+1,0);
-        if(n==1 || n==2) return 1;
-        if(n==0) return 0;
-        dp[1]=1,dp[2]=1;
+        int prev3=0,prev2=1,prev1=1,curr=0;
+        if(n==1||n==2) return 1;
         for(int i=3;i<n+1;i++){
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+            curr=prev1+prev2+prev3;
+            prev3=prev2;
+            prev2=prev1;
+            prev1=curr;
         }
-        return dp[n];
+        return curr;
     }
 };
