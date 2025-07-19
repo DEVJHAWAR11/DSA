31e596c37c56771f2e6e03b8737b3e2257147b35
DEVJHAWAR11 <156332863+DEVJHAWAR11@users.noreply.github.com>
2025-07-19 11:49:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (75.38%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -26,21 +26,26 @@ public:
     int numDecodings(string s) {
         int n=s.size();
         if(n==0) return 0;
+        
 
-        vector<int>dp(n+1,0); 
+        // vector<int>dp(n+1,0); 
         //dp[i] means no of wys to decode the first i chars of string s
-        dp[0]=1;
-        dp[1]=s[0]=='0'? 0 : 1;  //first char
+        int prev2=1;
+        int prev1=s[0]=='0'? 0 : 1;  //first char
+        int curr=0;
 
         for(int i=2;i<n+1;i++){
+            curr=0;
             if(s[i-1]!='0'){    //will only take from prev char if it is valid
-                dp[i]+=dp[i-1];
+                curr+=prev1;
             }
             int num=stoi(s.substr(i-2,2));
             if(num>=10 && num<=26){
-                dp[i]+=dp[i-2];
+                curr+=prev2;
             }
+            prev2=prev1;
+            prev1=curr;
         }
-        return dp[n];  
+        return prev1;  
     }
 };
