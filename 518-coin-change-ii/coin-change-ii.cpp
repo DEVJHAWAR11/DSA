5111df11022278c6ccba1f7d898419a973da0864
DEVJHAWAR11 <156332863+DEVJHAWAR11@users.noreply.github.com>
2025-07-19 14:15:06 +0530

    Time: 26 ms (38.12%) | Memory: 46.6 MB (6.72%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -18,3 +18,17 @@ public:
         return dp[n][amount];
     }
 };
+
+// class Solution {
+// public:
+//     int change(int amount, vector<int>& coins) {
+//         vector<unsigned int> dp(amount + 1, 0);
+//         dp[0] = 1;
+
+//         for (const int& coin : coins)
+//             for (int i = coin; i <= amount; i++)
+//                 dp[i] += dp[i - coin];
+
+//         return dp[amount];
+//     }
+// };
