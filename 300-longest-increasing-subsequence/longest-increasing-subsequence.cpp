25ead5d388091a4d1c1ed7861c768b4207469d12
DEVJHAWAR11 <156332863+DEVJHAWAR11@users.noreply.github.com>
2025-07-25 00:11:38 +0530

    Time: 250 ms (40.76%) | Memory: 296.4 MB (30.98%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -48,25 +48,48 @@
 
 
 
+// class Solution {
+// public:
+//     int solve(int curr, int prev, vector<int>& nums,vector<vector<int>>&dp) {
+//         if (curr == nums.size()) return 0;
+
+//         if(dp[curr][prev+1]!=-1) return dp[curr][prev+1];
+
+//         if (prev == -1 || nums[curr] > nums[prev])
+//             return dp[curr][prev+1] = max(
+//                 1 + solve(curr + 1, curr, nums,dp),  
+//                 solve(curr + 1, prev, nums,dp)   
+//             );
+//             else
+//                 return dp[curr][prev+1] = solve(curr + 1, prev, nums,dp);
+//     }
+
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+//         return solve(0, -1, nums,dp);
+//     }
+// };
+
+
+
 class Solution {
 public:
-    int solve(int curr, int prev, vector<int>& nums,vector<vector<int>>&dp) {
-        if (curr == nums.size()) return 0;
-
-        if(dp[curr][prev+1]!=-1) return dp[curr][prev+1];
-
-        if (prev == -1 || nums[curr] > nums[prev])
-            return dp[curr][prev+1] = max(
-                1 + solve(curr + 1, curr, nums,dp),  
-                solve(curr + 1, prev, nums,dp)   
-            );
-            else
-                return dp[curr][prev+1] = solve(curr + 1, prev, nums,dp);
-    }
 
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return solve(0, -1, nums,dp);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+
+        for(int curr=n-1; curr >=0;curr--){
+            for(int prev=curr-1;prev >=-1;prev--){
+                if(prev==-1 || nums[curr] > nums[prev]){
+                    dp[curr][prev+1]=max(1+dp[curr+1][curr+1],dp[curr+1][prev+1]);
+                }
+                else{
+                    dp[curr][prev+1]=dp[curr+1][prev+1];
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
