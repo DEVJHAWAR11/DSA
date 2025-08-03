31498b76cf850e8b8aca2506623a1ae3552b80ef
DEVJHAWAR11 <156332863+DEVJHAWAR11@users.noreply.github.com>
2025-08-03 10:06:28 +0530

    Time: 3 ms (50.00%) | Memory: 153.7 MB (44.44%) - LeetSync

--- 3954-maximum-balanced-shipments/maximum-balanced-shipments.cpp
+++ 3954-maximum-balanced-shipments/maximum-balanced-shipments.cpp
@@ -1,19 +1,34 @@
+// class Solution {
+// public:
+//     int maxBalancedShipments(vector<int>& weight) {
+//         int count=0;
+//         int i=0,j=1,maxi=0;
+//         while(i<j && j < weight.size()){
+//             if(weight[i] <= weight[j]){
+//                 maxi=max(maxi,weight[j]);
+//                 i++,j++;
+//             }
+//             else{
+//                 count++;
+//                 maxi=0;
+//                 i+=2,j+=2;
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+
 class Solution {
 public:
     int maxBalancedShipments(vector<int>& weight) {
-        int count=0;
-        int i=0,j=1,maxi=0;
-        while(i<j && j < weight.size()){
-            if(weight[i] <= weight[j]){
-                maxi=max(maxi,weight[j]);
-                i++,j++;
-            }
-            else{
-                count++;
-                maxi=0;
-                i+=2,j+=2;
+        int shipment = 0;
+        for(int i = 0; i < weight.size() - 1; i++){
+            if(weight[i] > weight[i+1]){
+                shipment++;
+                i++;
             }
         }
-        return count;
+        return shipment;
     }
 };
